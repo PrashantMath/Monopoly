85faaa55c86519e8d99f82f1a2926bbb56a7bf86
Prashant Mathapati <prashant.mathapati@mathworks.in>
2018-11-22 13:29:13 +0530

    Upload batch 1st mdofied

--- Upload.bat
+++ Upload.bat
@@ -2,5 +2,6 @@ set PRG=%JOB_NAME%
 set WSP=%WORKSPACE%\%PRG%
 set MROOT=%MATLABROOT%
 set Results_Dir=C:\My_Drive\Git_Rep\Mon_Results
+set Res=c:\MyDrive
 
 "%MROOT%\polyspace\bin\polyspace-results-repository.exe" -f -upload "%Results_Dir%\R_BF_%BUILD_NUMBER%" -server "localhost:12427" -prog "%PRG%" -product "BugFinder"
