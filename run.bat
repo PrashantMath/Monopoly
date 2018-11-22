a2c9edb7038cb4648be5df329a163bbe9b7bcdff
Prashant Mathapati <prashant.mathapati@mathworks.in>
2018-11-22 12:40:56 +0530

    New Files added

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 set PRG=%JOB_NAME%
 set WSP=%WORKSPACE%\%PRG%
 set MROOT=%MATLABROOT%
-"%MROOT%\polyspace\bin\polyspace-bug-finder-nodesktop.exe" -options-file "%WSP%\%PRG%_%BUILD_NUMBER%.opts" -options-file "%WORKSPACE%\Tools\target_%JOB_NAME%.opts" -date %BUILD_ID% -results-dir "%WSP%\R_BF_%BUILD_NUMBER%"
+set Results_Dir=C:\My_Drive\Git_Rep\Mon_Results
+"%MROOT%\polyspace\bin\polyspace-bug-finder-nodesktop.exe" -options-file "%WSP%\%PRG%_%BUILD_NUMBER%.opts" -options-file "%WORKSPACE%\target_%JOB_NAME%.opts" -date %BUILD_ID% -results-dir "%Results_Dir%\R_BF_%BUILD_NUMBER%"
